23ac06c91b1fcc83823b8a21860aa34215ba40ec
Satya010 <87820729+Satya010@users.noreply.github.com>
2023-04-19 23:17:50 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
+import './app.css';
+// import math from 'math';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=>{
+    const [result, setResult]= useState("");
+
+    const Click =(event)=>{
+        setResult(result+(event.target.name));
+    }
+
+    const Clear =()=>{
+        setResult("");
+    }
+
+    const Del = () => {
+        setResult(result.slice(0, -1));
+    }
+    const Answar = () => {
+        try {
+            setResult(eval(result).toString());
+            // setResult(math.calculate(result).toString());
+        } catch(err) {
+            setResult("Error")
+            // setResult((prevState) => ({ 
+            //     result: prevState.result
+            //  }))
+        }        
+    }
+
+    return(
+        <div className='container-box'>
+            <form>
+                <input type="text" value={result}/>
+            </form>
+
+            <div className='input-button'>
+                <button name='1' onClick={Click}>1</button>
+                <button name='2' onClick={Click}>2</button>
+                <button name='3' onClick={Click}>3</button>
+                <button className='special-button' name='+' onClick={Click}>+</button>
+                <button name='4' onClick={Click}>4</button>
+                <button name='5' onClick={Click}>5</button>
+                <button name='6' onClick={Click}>6</button>
+                <button className='special-button' name='-' onClick={Click}>-</button>
+                <button name='7' onClick={Click}>7</button>
+                <button name='8' onClick={Click}>8</button>
+                <button name='9' onClick={Click}>9</button>
+                <button className='special-button' name='*' onClick={Click}>*</button>
+                <button name='0' onClick={Click}>0</button>
+                <button name='.' onClick={Click}>.</button>
+                <button name='%' className='special-button' onClick={Click}>%</button>
+                <button className='special-button' name='/' onClick={Click}>/</button>
+                <button className='special-button' onClick={Clear} id='Clear'>Clear</button>
+                <button className='special-button' onClick={Del} id='Delete'>Del</button>
+                <button className='special-button' onClick={Answar} id='result'>Result</button>
+            </div>
+        </div>
+    );
 }
-
 export default App;
